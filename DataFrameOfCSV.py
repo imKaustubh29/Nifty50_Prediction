2a64b3434665403f3eff9a826c08a583080e66de
imKaustubh29 <51872311+imKaustubh29@users.noreply.github.com>
2024-11-10 21:54:08 +0100

    Save uncommitted changes

---
+++ DataFrameOfCSV.py
@@ -0,0 +1,6 @@
+# Load the dataset
+data = pd.read_csv('/Users/kaustubh/Desktop/VSCode_Proj/Nifty50_Prediction/NIFTY50_data.csv')
+
+# Print the first few rows and the column names
+print(data.head())
+print(data.columns)
